33f44e0211ab659e55676275db23049d3ec40775
goeun208 <qop341@gmail.com>
2022-03-31 21:17:56 +0900

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
   return (
   <div>
     <nav className={styles.navbar}>
-      <div><Link to="/" className={styles.logo} >Front-end study</Link></div>
+      <div><Link to="/" className={styles.logo} ></Link></div>
       <ul className={styles.navLinks}>
         <li><NavLink to="/video" activeStyle={activeStyle} className={styles.link}>참고 강의</NavLink></li>
         <li><NavLink to="/problems" activeStyle={activeStyle} className={styles.link}>푼 문제들</NavLink></li>
